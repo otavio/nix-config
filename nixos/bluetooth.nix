cb511481e5fab9750ee230c71d5a188da9f14f4a
Otavio Salvador <otavio@ossystems.com.br>
2020-11-28 11:36:48 -0300

    nixos: bluetooth: Add extra codecs to improve audio
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- nixos/bluetooth.nix
+++ nixos/bluetooth.nix
@@ -4,6 +4,7 @@
   hardware.pulseaudio = {
     enable = true;
     package = pkgs.pulseaudioFull;
+    extraModules = [ pkgs.pulseaudio-modules-bt ];
     extraConfig = ''
       load-module module-switch-on-connect
     '';
