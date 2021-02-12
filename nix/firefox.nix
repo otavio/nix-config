5d2a309f374fd630765738d9c07cba9e40685dff
Otavio Salvador <otavio@ossystems.com.br>
2021-02-12 00:56:05 -0300

    firefox: Stop handling addons
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- nix/firefox.nix
+++ nix/firefox.nix
@@ -1,17 +1,7 @@
 { pkgs, ... }:
-let
-  nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-    inherit pkgs;
-  };
-in
 {
   programs.firefox = {
     enable = true;
-
-    extensions = with nur.repos.rycee.firefox-addons; [
-      lastpass-password-manager
-      vimium
-    ];
 
     profiles = {
       otavio = {
