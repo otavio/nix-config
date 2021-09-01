55cf3bb32f77f4e45bcf9b97234315f90bf2b54a
Otavio Salvador <otavio@ossystems.com.br>
2021-09-01 15:59:16 -0300

    nixos: bluetooth: use pipewire
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- nixos/bluetooth.nix
+++ nixos/bluetooth.nix
@@ -1,14 +1,53 @@
 { config, pkgs, ... }:
 
 {
-  hardware.pulseaudio = {
-    enable = true;
-    package = pkgs.pulseaudioFull;
+  sound.enable = true;
 
-    extraModules = [ pkgs.pulseaudio-modules-bt ];
-    extraConfig = ''
-      load-module module-switch-on-connect
-    '';
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+    media-session.config.bluez-monitor.rules = [
+      {
+        # Matches all cards
+        matches = [ { "device.name" = "~bluez_card.*"; } ];
+        actions = {
+          "update-props" = {
+            "bluez5.auto-connect" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+          };
+        };
+      }
+      {
+        matches = [
+          # Matches all sources
+          { "node.name" = "~bluez_input.*"; }
+          # Matches all outputs
+          { "node.name" = "~bluez_output.*"; }
+        ];
+        actions = {
+          "node.pause-on-idle" = false;
+        };
+      }
+      # {
+      # # Allow a phone etc to use this computer as a bluetooth "speaker"
+      #  matches = [ { "device.name" = "bluez_card.PHONE_MAC_HERE"; } ];
+      #    actions = {
+      #      "update-props" = {
+      #       "bluez5.auto-connect" = [ "hfp_ag" "hsp_ag" "a2dp_source" ];
+      #       "bluez5.a2dp-source-role" = "input";
+      #     };
+      #  };
+      #}
+    ];
   };
 
   hardware.bluetooth = {
