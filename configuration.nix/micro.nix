37267ff973dbe898233e61362e7270f79e2aae11
Otavio Salvador <otavio@ossystems.com.br>
2021-09-09 18:30:07 -0300

    configuration.nix: micro: add X11 modeline for UltraWide
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- configuration.nix/micro.nix
+++ configuration.nix/micro.nix
@@ -20,10 +20,28 @@
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
     kernelPackages = pkgs.linuxPackages_latest;
+    kernelParams = [ "video=HDMI-A-1:2560x1080"  ];
   };
 
   hardware.cpu.intel.updateMicrocode = true;
   hardware.video.hidpi.enable = false;
+  hardware.enableAllFirmware = true;
+
+  services.xserver = {
+    xrandrHeads = [
+      { output = "HDMI-1";
+        primary = true;
+        monitorConfig = ''
+          Modeline "2560x1080_60.00"  230.76  2560 2728 3000 3440  1080 1081 1084 1118  -HSync +Vsync
+          Option "PreferredMode" "2560x1080"
+          Option "Position" "0 0"
+        '';
+      }
+    ];
+    resolutions = [
+      { x = 2560; y = 1080; }
+    ];
+  };
 
   powerManagement.cpuFreqGovernor = "performance";
 
