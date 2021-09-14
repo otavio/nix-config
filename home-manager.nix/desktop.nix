c24b00a0268b78d7ce3aacde4ac68558fc91a046
Otavio Salvador <otavio@ossystems.com.br>
2021-09-14 20:48:13 -0300

    general: small code style rework
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- home-manager.nix/desktop.nix
+++ home-manager.nix/desktop.nix
@@ -4,12 +4,12 @@
   imports = [
     ../nix/base.nix
     ../nix/desktop.nix
+    ../nix/firefox.nix
     ../nix/go.nix
     ../nix/gtk.nix
     ../nix/i3.nix
     ../nix/nix.nix
     ../nix/rust.nix
     ../nix/zsh.nix
-    ../nix/firefox.nix
   ];
 }
