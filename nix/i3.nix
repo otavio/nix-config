otavio/nix-config

commit bb3ee6f938d4489b0eec6517a91ada7bf55a3e57

nix: i3: assign Contrl+space to hide dunst notification
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

nix/i3.nix
@@ -6,6 +6,8 @@ let
   terminal = "i3-sensible-terminal --class=term";
   menu = "i3-sensible-terminal -t 'fzf-menu' --class 'fzf-menu' -e fzf-menu";
 
+  dunstCloseNotification = "dunstctl close";
+
   shellWs = "1: shell ";
   editorWs = "2: editor ";
   browserWs = "3: browser ";
@@ -79,6 +81,9 @@ in
         "${modifier}+Shift+0" = "move container to workspace number ${triviaWs}";
 
         "Print" = "exec flameshot gui";
+
+        # Hide dunst notification.
+        "Control+space" = "exec ${dunstCloseNotification}";
       };
 
       window.commands = [

commit cd24d272ac6fe49284b2d25d5a8f903ba6e6c8ce

nix: i3: change Control+space to Control+Shift+space keybind
We need Control+space to start region mark inside Emacs.

Fixes: bb3ee6f ("nix: i3: assign Contrl+space to hide dunst notification")
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

nix/i3.nix
@@ -83,7 +83,7 @@ in
         "Print" = "exec flameshot gui";
 
         # Hide dunst notification.
-        "Control+space" = "exec ${dunstCloseNotification}";
+        "Control+Shift+space" = "exec ${dunstCloseNotification}";
       };
 
       window.commands = [