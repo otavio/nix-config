b2e04746553bb21312a2da9945d5407e34a2d291
Otavio Salvador <otavio@ossystems.com.br>
2022-04-27 13:32:05 -0300

    Remove backport of tmuxp and libtmux
    The PR (https://github.com/NixOS/nixpkgs/pull/170276) has been merged
    and we already have it available in the `flake.lock` so we can drop the
    overlayed packages now.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,12 +1,2 @@
 final: prev:
-{
-  # PR sent - https://github.com/NixOS/nixpkgs/pull/170276
-  python3 = prev.python3.override {
-    packageOverrides = final: prev: {
-      libtmux = final.callPackage ./libtmux { };
-    };
-  };
-
-  tmuxp = prev.callPackage ./tmuxp { };
-  python3Packages = final.python3.pkgs;
-} // (import ../pkgs) { pkgs = final; }
+{ } // (import ../pkgs) { pkgs = final; }
