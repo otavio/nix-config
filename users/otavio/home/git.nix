4431668eb3df0983d84139458684863136e26013
Otavio Salvador <otavio@ossystems.com.br>
2022-05-09 09:55:57 -0300

    users: otavio: convert Git configuration to expression
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- users/otavio/home/git.nix
+++ users/otavio/home/git.nix
@@ -1,13 +1,11 @@
 { pkgs, ... }:
 {
   home.file = {
-    ".gitconfig".source = ./git/config;
     ".gitaliases".source = ./git/aliases;
     ".patman".source = ./git/patman;
   };
 
   home.packages = with pkgs; [
-    git
     git-review
     git-secret
     gitAndTools.delta
@@ -15,4 +13,33 @@
     gh
     patman
   ];
+
+  programs.git = {
+    enable = true;
+
+    userName = "Otavio Salvador";
+    userEmail = "otavio@ossystems.com.br";
+
+    aliases = {
+      st = "status";
+      wlog = "log --color-words";
+      wdiff = "diff --color-words";
+      wshow = "show --color-words";
+      signoff-rebase = "!GIT_SEQUENCE_EDITOR='sed -i -re s/^pick/e/' sh -c 'git rebase -i $1 && while git rebase --continue; do git commit --amend --signoff --no-edit; done' -";
+    };
+
+    delta.enable = true;
+
+    ignores = [ ".direnv" ];
+
+    extraConfig = {
+      pull = { rebase = true; };
+      rebase = {
+        autoStash = true;
+        autoSquash = true;
+        abbreviateCommands = true;
+        missingCommitsCheck = "warn";
+      };
+    };
+  };
 }
