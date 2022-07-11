d5285c4aa4c02962db35cde153c64b41cf310395
Otavio Salvador <otavio@ossystems.com.br>
2022-07-11 09:27:22 -0300

    users: otavio: zsh: base16-shell: 1af738db -> 41848241
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- users/otavio/home/zsh.nix
+++ users/otavio/home/zsh.nix
@@ -5,8 +5,8 @@ let
     src = pkgs.fetchFromGitHub {
       owner = "base16-project";
       repo = "base16-shell";
-      rev = "1af738dbb8fb6dbfd0402502ed47937ef4ebd461";
-      sha256 = "sha256-ZlZpMvoljpfeTuOfTrF9qEnoGnkpxGjznd54y+deYwA=";
+      rev = "41848241532fd60cdda222cc8f7b2bbead9fb50d";
+      sha256 = "sha256-rkgH8J6RgI3ej04z4gPFHMabaBRZKeaXIHhk0HxXMHo=";
     };
 
     installPhase = ''
