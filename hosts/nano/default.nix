7ee3848fdba7e2d3ea215c18d21269dda747b849
Otavio Salvador <otavio@ossystems.com.br>
2022-07-24 19:02:55 -0300

    hosts: remove comment about `useDHCP` as it is true by default
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- hosts/nano/default.nix
+++ hosts/nano/default.nix
@@ -18,9 +18,6 @@
 
   powerManagement.cpuFreqGovernor = "performance";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
   networking.useDHCP = false;
 
   networking.networkmanager.enable = true;
