f88efd86ac97057ebdd71c527969741a8f49cfdb
Otavio Salvador <otavio@ossystems.com.br>
2022-08-13 16:17:42 -0300

    users: otavio: git: add aliases for local branches pruning
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- users/otavio/home/git.nix
+++ users/otavio/home/git.nix
@@ -25,6 +25,8 @@
       wdiff = "diff --color-words";
       wshow = "show --color-words";
       signoff-rebase = "!GIT_SEQUENCE_EDITOR='sed -i -re s/^pick/e/' sh -c 'git rebase -i $1 && while git rebase --continue; do git commit --amend --signoff --no-edit; done' -";
+      prune-merged-branches = "git branch --merged $1 | grep -v \"^\ * $1 \" | xargs -n 1 -r git branch -d";
+      prune-local-branches = "git branch -vv | grep ': gone]' | grep -v '\\*' | awk '{ print $1; }' | xargs -r git branch -d";
     };
 
     delta = {
