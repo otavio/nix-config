747c62fb442bcf1c0d336e89ac8784b4ba4bd624
Otavio Salvador <otavio@ossystems.com.br>
2022-08-15 10:06:09 -0300

    flake: add cachix cache
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "Otavio Salvador's NixOS/Home Manager config";
 
+  nixConfig.extra-substituters = [
+    "https://nix-community.cachix.org"
+    "https://otavio-nix-config.cachix.org"
+  ];
+  nixConfig.extra-trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "otavio-nix-config.cachix.org-1:4HXl0KPGJ0+tkTUn/0tHRpz1wJst9MxovLjKbsPnqS4="
+  ];
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
