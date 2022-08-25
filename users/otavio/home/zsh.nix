d4e06c6da23d5dcffe1a80b8b379f8849b64212f
Otavio Salvador <otavio@ossystems.com.br>
2022-08-25 14:53:55 -0300

    users: otavio: zsh: improve prompt so we show nix-shell as indicator
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- users/otavio/home/zsh.nix
+++ users/otavio/home/zsh.nix
@@ -79,6 +79,28 @@ in
     initExtraFirst = ''
       source ${pkgs.grml-zsh-config}/etc/zsh/zshrc
 
+      # Prompt modifications.
+      #
+      # In current grml zshrc, changing `$PROMPT` no longer works,
+      # and `zstyle` is used instead, see:
+      # https://unix.stackexchange.com/questions/656152/why-does-setting-prompt-have-no-effect-in-grmls-zshrc
+
+      # Disable the grml `sad-smiley` on the right for exit codes != 0;
+      # it makes copy-pasting out terminal output difficult.
+      # Done by setting the `items` of the right-side setup to the empty list
+      # (as of writing, the default is `items sad-smiley`).
+      # See: https://bts.grml.org/grml/issue2267
+      zstyle ':prompt:grml:right:setup' items
+
+      # Add nix-shell indicator that makes clear when we're in nix-shell.
+      # Described in: http://bewatermyfriend.org/p/2013/003/
+      nix_shell_prompt() {
+          REPLY=''${IN_NIX_SHELL+"(nix-shell)"}
+      }
+      grml_theme_add_token nix-shell-indicator -f nix_shell_prompt '%F{magenta}' '%f'
+      zstyle ':prompt:grml:left:setup' items rc change-root user at host path vcs \
+                                             nix-shell-indicator percent
+
       # Base16 Shell
       BASE16_SHELL_PATH="${base16-shell}/share/base16-shell"
       [ -n "$PS1" ] && \
