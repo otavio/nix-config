d88ef37bca6e77a291832c433ade42d6ce3e35ed
Otavio Salvador <otavio@ossystems.com.br>
2023-04-06 23:14:36 -0300

    hosts: features: optional: simplify the bluetooth module
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- hosts/features/optional/bluetooth.nix
+++ hosts/features/optional/bluetooth.nix
@@ -1,27 +1,6 @@
-{ config, pkgs, ... }:
-
 {
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-
-    alsa.enable = true;
-    pulse.enable = true;
-  };
-
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
-  };
-
-  environment.etc = {
-    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-      bluez_monitor.properties = {
-        ["bluez5.enable-sbc-xq"] = true,
-        ["bluez5.enable-msbc"] = true,
-        ["bluez5.enable-hw-volume"] = true,
-        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-      }
-    '';
   };
 }
