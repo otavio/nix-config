5c741e86a30449910b7087c0f89a438b28963266
Otavio Salvador <otavio@ossystems.com.br>
2023-04-08 16:47:50 -0300

    users: bruna: system: split out restic
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- users/bruna/system/default.nix
+++ users/bruna/system/default.nix
@@ -1,8 +1,9 @@
 { config, pkgs, ... }:
-let
-  homeDirectory = "/home/bruna";
-in
 {
+  imports = [
+    ./restic.nix
+  ];
+
   users.users.bruna = {
     description = "Bruna C. Tessmer Salvador";
 
@@ -18,36 +19,5 @@ in
 
     # Default - used for bootstrapping.
     password = "pw";
-  };
-
-  sops.secrets = {
-    "backup/credentials" = { };
-    "backup/repository" = { };
-    "backup/password" = { };
-  };
-
-  services.restic.backups = {
-    wasabi = {
-      user = "root";
-      initialize = true;
-
-      environmentFile = "${config.sops.secrets."backup/credentials".path}";
-      repositoryFile = "${config.sops.secrets."backup/repository".path}";
-      passwordFile = "${config.sops.secrets."backup/password".path}";
-
-      paths = [ "${homeDirectory}" ];
-
-      pruneOpts = [
-        "--keep-daily 7"
-        "--keep-weekly 5"
-        "--keep-monthly 12"
-        "--keep-yearly 3"
-      ];
-
-      extraBackupArgs = [
-        "--exclude-caches"
-        "--exclude-if-present .backup-ignore"
-      ];
-    };
   };
 }
---
+++ users/bruna/system/restic.nix
@@ -0,0 +1,34 @@
+{ config, ... }:
+
+{
+  sops.secrets = {
+    "backup/credentials" = { };
+    "backup/repository" = { };
+    "backup/password" = { };
+  };
+
+  services.restic.backups = {
+    wasabi = {
+      user = "root";
+      initialize = true;
+
+      environmentFile = config.sops.secrets."backup/credentials".path;
+      repositoryFile = config.sops.secrets."backup/repository".path;
+      passwordFile = config.sops.secrets."backup/password".path;
+
+      paths = [ config.users.users.bruna.home ];
+
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 5"
+        "--keep-monthly 12"
+        "--keep-yearly 3"
+      ];
+
+      extraBackupArgs = [
+        "--exclude-caches"
+        "--exclude-if-present .backup-ignore"
+      ];
+    };
+  };
+}
