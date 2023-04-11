6bc2a424fdbabbde2fc3ace2a2df01397f9a1956
Otavio Salvador <otavio@ossystems.com.br>
2023-04-10 22:17:50 -0300

    hosts: micro: split out wireguard module
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

---
+++ hosts/micro/wireguard.nix
@@ -0,0 +1,28 @@
+{ config, ... }:
+
+{
+  sops.secrets."wireguard/micro/private-key" = { };
+  networking.wireguard.interfaces = {
+    wg0 = {
+      ips = [ "10.10.1.3/32" ];
+      privateKeyFile = config.sops.secrets."wireguard/micro/private-key".path;
+      peers = [
+        {
+          publicKey = "3cJEElR2e9ClzNHHqDkNgqulOsw3u5OdKnKj3bd4K1c=";
+          allowedIPs = [
+            "10.4.0.0/16"
+            "10.5.0.0/16"
+          ];
+          endpoint = "ossystems.ddns.net:51820";
+          persistentKeepalive = 25;
+          dynamicEndpointRefreshSeconds = 30;
+        }
+      ];
+      postSetup = ''
+        resolvectl dns    wg0 10.5.1.254
+        resolvectl domain wg0 "~lab.ossystems"
+        resolvectl dnssec wg0 false
+      '';
+    };
+  };
+}
