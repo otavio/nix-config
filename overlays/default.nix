8852e00940cd1ad44c33f0eb38aaeb6b248260eb
Otavio Salvador <otavio@ossystems.com.br>
2023-04-18 08:26:56 -0300

    pkgs: fzf: ensure all binaries are explicit in the scripts
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,4 +2,16 @@
 {
   # Adds my custom packages
   additions = final: prev: import ../pkgs { pkgs = final; };
+
+  modifications = final: prev: {
+    fzf = prev.fzf.overrideAttrs (oa: {
+      # https://github.com/NixOS/nixpkgs/pull/226847
+      postInstall = oa.postInstall + ''
+        substituteInPlace $out/share/fzf/completion.* $out/share/fzf/key-bindings.* \
+          --replace "\"fzf\"" "\"$out/bin/fzf\"" \
+          --replace "fzf-tmux " "$out/bin/fzf-tmux " \
+          --replace "fzf " "$out/bin/fzf "
+      '';
+    });
+  };
 }
