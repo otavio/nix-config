fa278977b663dfb6e3cbab9f771d63f1ab562ec1
Otavio Salvador <otavio@ossystems.com.br>
2023-04-20 15:13:06 -0300

    users: otavio: base: drop topgrade as it is unused in normal workflow
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- users/otavio/home/base.nix
+++ users/otavio/home/base.nix
@@ -81,25 +81,4 @@ in
     ".tmuxp" = { source = ./tmux/tmuxp; recursive = true; };
     ".yocto/site.conf".source = ./yocto/site.conf;
   };
-
-  programs.topgrade = {
-    enable = true;
-    settings = {
-      assume_yes = true;
-      disable = [
-        "emacs"
-        "flutter"
-        "home_manager"
-        "nix"
-        "node"
-        "pip3"
-      ];
-      set_title = false;
-      skip_notify = true;
-      cleanup = true;
-      pre_commands = {
-        "nix-config" = "cd ~/src/nix-config && git pull --rebase --autostash";
-      };
-    };
-  };
 }
