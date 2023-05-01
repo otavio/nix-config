ea41e41bc4f4c1fb144b14b57369cd476c06cbea
Otavio Salvador <otavio@ossystems.com.br>
2023-05-01 17:49:30 -0300

    hosts: features: optional: pipewire: extend codecs supported
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- hosts/features/optional/pipewire.nix
+++ hosts/features/optional/pipewire.nix
@@ -1,11 +1,29 @@
 {
   security.rtkit.enable = true;
   hardware.pulseaudio.enable = false;
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    wireplumber = {
+      enable = true;
+    };
+  };
+
+  environment = {
+    etc = {
+      "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+        bluez_monitor.properties = {
+            ["bluez5.enable-sbc-xq"]    = true,
+            ["bluez5.enable-msbc"]      = true,
+            ["bluez5.enable-hw-volume"] = true,
+            ["bluez5.headset-roles"]    = "[hsp_hs hsp_ag hfp_hf hfp_ag]",
+            ["bluez5.codecs"]           = "[aptx aptx_hd sbc sbc_xq]",
+        }
+      '';
+    };
   };
 }
