d751e05530bb2a9dd6a15a59ca486510f0855824
Otavio Salvador <otavio@ossystems.com.br>
2023-05-02 13:05:14 -0300

    hosts: micro: set IO scheduler
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- hosts/micro/default.nix
+++ hosts/micro/default.nix
@@ -36,6 +36,15 @@
     kernelParams = [ "video=HDMI-A-1:2560x1080" ];
   };
 
+  services.udev.extraRules = ''
+    # Set scheduler for NVMe
+    ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/scheduler}="none"
+    # Set scheduler for SSD and eMMC
+    ACTION=="add|change", KERNEL=="sd[a-z]|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="mq-deadline"
+    # Set scheduler for rotating disks
+    ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"
+  '';
+
   services.xserver = {
     xrandrHeads = [{
       output = "HDMI-1";
