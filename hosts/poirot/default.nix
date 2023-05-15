47f3f8b263c70d519b4666b41321b4382095388b
Otavio Salvador <otavio@ossystems.com.br>
2023-05-15 13:11:48 -0300

    hosts: poirot: add "systemd.gpt_auto=0" to boot.kernelParams
    Fixes:
    
    ,----[ dmesg error ]
    | [91755.057901] systemd-gpt-auto-generator[13167]: (The boot loader did
    |     not set EFI variable LoaderDevicePartUUID.)
    `----
    
    Refs: https://github.com/NixOS/nixpkgs/issues/35681
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- hosts/poirot/default.nix
+++ hosts/poirot/default.nix
@@ -22,6 +22,8 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
+  boot.kernelParams = [ "systemd.gpt_auto=0" ];
+
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
