1f5a05623af6da8e5d38a51e400b9dc70aad1fa8
Otavio Salvador <otavio@ossystems.com.br>
2023-06-09 19:26:51 -0300

    hosts: features: optional: podman: conditionally enable docker compat
    The podman compatibility only if docker itself is not enabled.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- hosts/features/optional/podman.nix
+++ hosts/features/optional/podman.nix
@@ -1,7 +1,12 @@
+{ config, ... }:
+let
+  dockerEnabled = config.virtualisation.docker.enable;
+in
 {
   virtualisation.podman = {
     enable = true;
-    dockerCompat = true;
-    dockerSocket.enable = true;
+    dockerCompat = !dockerEnabled;
+    dockerSocket.enable = !dockerEnabled;
+    defaultNetwork.settings.dns_enabled = true;
   };
 }
