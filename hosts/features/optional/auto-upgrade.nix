2338a617942490716bd24d848f11853333bc8ffa
Otavio Salvador <otavio@ossystems.com.br>
2023-06-09 19:23:38 -0300

    hosts: features: optional: auto-upgrade: restrict to run on clean trees
    Only enable auto upgrade if current config came from a clean tree This
    avoids accidental auto-upgrades when working locally.
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- hosts/features/optional/auto-upgrade.nix
+++ hosts/features/optional/auto-upgrade.nix
@@ -1,7 +1,12 @@
-{ config, ... }:
+{ inputs, ... }:
+let
+  # Only enable auto upgrade if current config came from a clean tree
+  # This avoids accidental auto-upgrades when working locally.
+  isClean = inputs.self ? rev;
+in
 {
   system.autoUpgrade = {
-    enable = true;
+    enable = isClean;
     dates = "hourly";
     flags = [ "--refresh" ];
     flake = "github:otavio/nix-config";
