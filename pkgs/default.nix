dff8fed374bfcf7610c8da26ca9280278b7ca647
Otavio Salvador <otavio@ossystems.com.br>
2023-06-13 17:50:44 -0300

    users: otavio: desktop: move flameshotOcr to pkgs
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,20 @@
 { pkgs }:
+let
+  flameshotOcrForLang = lang: pkgs.writeScriptBin "flameshot-ocr-${lang}" ''
+    # The sleep is required to give time for the fzf-menu to disappear before opening flameshot.
+    sleep 0.1
+
+    ${pkgs.flameshot}/bin/flameshot gui -r | \
+       ${pkgs.tesseract}/bin/tesseract -l ${lang} - - | \
+       ${pkgs.xclip}/bin/xclip -sel clip
+  '';
+in
 {
+  flameshotOcr = {
+    eng = flameshotOcrForLang "eng";
+    por = flameshotOcrForLang "por";
+  };
+
   base16-shell = pkgs.callPackage ./base16-shell { };
   bitbake-completion = pkgs.callPackage ./bitbake-completion { };
   mods = pkgs.callPackage ./mods { };
