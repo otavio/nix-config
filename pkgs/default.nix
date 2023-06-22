f965afbf65fa04efd0e1adb8a736b7406c947563
Otavio Salvador <otavio@ossystems.com.br>
2023-06-22 17:58:46 -0300

    pkgs: mods: remove as it exists in upstream
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -17,7 +17,6 @@ in
 
   base16-shell = pkgs.callPackage ./base16-shell { };
   bitbake-completion = pkgs.callPackage ./bitbake-completion { };
-  mods = pkgs.callPackage ./mods { };
   kube-ps1 = pkgs.callPackage ./kube-ps1 { };
   pa-applet = pkgs.callPackage ./pa-applet { };
   patman = pkgs.callPackage ./patman { };
