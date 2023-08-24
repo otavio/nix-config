ff19b82b0ad4a84c981568902e46fcada5d84b97
Otavio Salvador <otavio@ossystems.com.br>
2023-08-23 22:48:31 -0300

    users: otavio: xdg: add workaround for mimeapps.list override
    Refs: https://github.com/nix-community/home-manager/issues/1213
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- users/otavio/home/features/xdg/default.nix
+++ users/otavio/home/features/xdg/default.nix
@@ -4,5 +4,11 @@ _:
   xdg = {
     enable = true;
     mimeApps.enable = true;
+
+    # We force the override so we workaround the error below:
+    #   Existing file '/.../.config/mimeapps.list' is in the way of
+    #   '/nix/store/...-home-manager-files/.config/mimeapps.list'
+    # Issue: https://github.com/nix-community/home-manager/issues/1213
+    configFile."mimeapps.list".force = true;
   };
 }
