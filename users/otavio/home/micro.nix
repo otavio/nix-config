e4a02e2cd2fb7826ee4f7e570d9712b37220154a
Otavio Salvador <otavio@ossystems.com.br>
2024-02-13 15:25:43 -0300

    aa
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- users/otavio/home/micro.nix
+++ users/otavio/home/micro.nix
@@ -10,15 +10,21 @@
     ./features/flameshot
     ./features/gpg
     ./features/gtk
-    ./features/i3wm
+    ./features/swaywm
     ./features/irssi
     ./features/ossystems-specific
-    ./features/parcellite
     ./features/unclutter
     ./features/xdg
     ./features/zathura
     ./features/zsh
   ];
+
+  wayland.windowManager.sway.config = {
+    output."LG Electronics LG ULTRAWIDE 0x01010101" = {
+      modeline = "230.76  2560 2728 3000 3440  1080 1081 1084 1118  -HSync +Vsync";
+      background = "#000000 solid_color";
+    };
+  };
 
   # FIXME: Avoid Vulkan use in GTK rendering as it causes GPU error, see:
   # https://gitlab.freedesktop.org/drm/nouveau/-/issues/381
@@ -39,7 +45,7 @@
       name = "scrcpy";
       runtimeInputs = with pkgs; [ scrcpy ];
       text = ''
-        scrcpy -M
+        scrcpy -M --no-audio
       '';
     })
 
@@ -55,4 +61,22 @@
     slack
     tdesktop
   ];
+
+  systemd.user.services = {
+    xwaylandvideobridge = {
+      Unit = {
+        Description = "Tool to make it easy to stream wayland windows and screens to existing applications running under Xwayland";
+      };
+
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.xwaylandvideobridge}/bin/xwaylandvideobridge";
+        Restart = "on-failure";
+      };
+
+      Install = {
+        WantedBy = [ "sway-session.target" ];
+      };
+    };
+  };
 }
