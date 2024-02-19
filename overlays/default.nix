9040d8ca56d8c4c9f7514235557a4f0715a38e38
Otavio Salvador <otavio@ossystems.com.br>
2024-02-19 16:11:48 -0300

    tmux: backport fix for 3.4 for split-window regression
    Refs: https://github.com/NixOS/nixpkgs/pull/289789
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,5 +12,15 @@
           --replace "fzf " "$out/bin/fzf "
       '';
     });
+
+    tmux = prev.tmux.overrideAttrs (oa: {
+      # https://github.com/NixOS/nixpkgs/pull/289789
+      patches = (oa.patches or [ ]) ++ [
+        (prev.fetchpatch {
+          url = "https://github.com/tmux/tmux/commit/2d1afa0e62a24aa7c53ce4fb6f1e35e29d01a904.diff";
+          hash = "sha256-mDt5wy570qrUc0clGa3GhZFTKgL0sfnQcWJEJBKAbKs=";
+        })
+      ];
+    });
   };
 }
