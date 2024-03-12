otavio/nix-config

commit b4c35fad47caa80c895d02f1eadd99357e204bef

users: otavio: flameshot: enable wayland support
Refs: https://github.com/NixOS/nixpkgs/pull/287307
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

users/otavio/home/features/flameshot/default.nix
@@ -15,7 +15,16 @@ let
   };
 in
 {
-  services.flameshot.enable = true;
+  services.flameshot = {
+    enable = true;
+    # Refs: https://github.com/NixOS/nixpkgs/pull/287307
+    package = pkgs.flameshot.overrideAttrs (oldAttrs: {
+      buildInputs = oldAttrs.buildInputs ++ [ pkgs.libsForQt5.kguiaddons ];
+      cmakeFlags = [
+        "-DUSE_WAYLAND_CLIPBOARD=true"
+      ];
+    });
+  };
 
   home.packages = [
     flameshotOcr.eng

commit 1361654cde554839895f748b0d61f2e21e30e630

users: otavio: flameshot: 12.1.0 -> 12.1.0-unstable-2023-11-25
Fixes: b4c35fa ("users: otavio: flameshot: enable wayland support")
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

users/otavio/home/features/flameshot/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 let
   flameshotOcrForLang = lang: pkgs.writeScriptBin "flameshot-ocr-${lang}" ''
     # The sleep is required to give time for the fzf-menu to disappear before opening flameshot.
@@ -13,17 +13,20 @@ let
     eng = flameshotOcrForLang "eng";
     por = flameshotOcrForLang "por";
   };
+
+  flameshot = pkgs.libsForQt5.callPackage ./flameshot.nix { };
 in
 {
   services.flameshot = {
     enable = true;
-    # Refs: https://github.com/NixOS/nixpkgs/pull/287307
-    package = pkgs.flameshot.overrideAttrs (oldAttrs: {
-      buildInputs = oldAttrs.buildInputs ++ [ pkgs.libsForQt5.kguiaddons ];
-      cmakeFlags = [
-        "-DUSE_WAYLAND_CLIPBOARD=true"
-      ];
-    });
+    package = flameshot;
+    settings = {
+      General = {
+        disabledTrayIcon = true;
+        showDesktopNotification = false;
+        savePath = "${config.home.homeDirectory}/Downloads";
+      };
+    };
   };
 
   home.packages = [

users/otavio/home/features/flameshot/flameshot.nix
@@ -1,0 +1,48 @@
+{ mkDerivation
+, lib
+, fetchFromGitHub
+, qtbase
+, cmake
+, qttools
+, qtsvg
+, kguiaddons
+, grim
+, nix-update-script
+}:
+
+mkDerivation {
+  pname = "flameshot";
+  version = "12.1.0-unstable-2023-11-25";
+
+  src = fetchFromGitHub {
+    owner = "flameshot-org";
+    repo = "flameshot";
+    rev = "3d21e4967b68e9ce80fb2238857aa1bf12c7b905";
+    sha256 = "sha256-OLRtF/yjHDN+sIbgilBZ6sBZ3FO6K533kFC1L2peugc=";
+  };
+
+  passthru = {
+    updateScript = nix-update-script { };
+  };
+
+  nativeBuildInputs = [ cmake qttools qtsvg ];
+  buildInputs = [ qtbase kguiaddons ];
+  cmakeFlags = [
+    "-DUSE_WAYLAND_CLIPBOARD=true"
+    "-DUSE_WAYLAND_GRIM=true"
+  ];
+
+  postInstall = ''
+    wrapProgram $out/bin/flameshot \
+      --prefix PATH : ${lib.makeBinPath [ grim ]}
+  '';
+
+  meta = with lib; {
+    description = "Powerful yet simple to use screenshot software";
+    homepage = "https://github.com/flameshot-org/flameshot";
+    mainProgram = "flameshot";
+    maintainers = with maintainers; [ scode oxalica ];
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux ++ platforms.darwin;
+  };
+}