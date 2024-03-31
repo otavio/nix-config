c8340891bf414403a4cb919205e3d816c8d06b64
Otavio Salvador <otavio@ossystems.com.br>
2024-03-31 17:07:06 -0300

    users: otavio: disable symlink of nix-config to .config/home-manager
    Nix 1.19.2 fails to build a derivation containing a symlink to store path.
    
    ```nix
    { local ? "/home/rodney/ops/nixpkgs"
    , pkgs ? import local {}
    }: rec {
      direct-symlink = pkgs.runCommand "direct-symlink" {} ''
        ln -vs ${local}/.version $out
      '';
      indirect-symlink = pkgs.runCommand "indirect-symlink" {} ''
        ln -vs ${direct-symlink} $out
      '';
    }
    ```
    
    Refs:
    - https://github.com/nix-community/home-manager/issues/4692
    - https://github.com/NixOS/nix/issues/9579
    
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- users/otavio/home/generic.nix
+++ users/otavio/home/generic.nix
@@ -1,7 +1,14 @@
 {
   imports = [
     ./features/global
-    ./features/home-manager-switch
+
+    # Nix 1.19.2 fails to build a derivation containing a symlink to store path.
+    #
+    # Refs:
+    # - https://github.com/nix-community/home-manager/issues/4692
+    # - https://github.com/NixOS/nix/issues/9579
+    #./features/home-manager-switch
+
     ./features/emacs
   ];
 }
