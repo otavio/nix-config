b64bfdb234784d6de159560bd30a86860a3e6df2
Otavio Salvador <otavio@ossystems.com.br>
2024-06-01 18:02:41 -0300

    hosts: features: nix: drop cache.garnix.io
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- hosts/features/required/nix.nix
+++ hosts/features/required/nix.nix
@@ -4,7 +4,6 @@
     settings = {
       substituters = [
         "https://nix-community.cachix.org"
-        "https://cache.garnix.io"
         "https://otavio-nix-config.cachix.org"
         "https://numtide.cachix.org"
       ];
@@ -15,7 +14,6 @@
 
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "otavio-nix-config.cachix.org-1:4HXl0KPGJ0+tkTUn/0tHRpz1wJst9MxovLjKbsPnqS4="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       ];
