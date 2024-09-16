1760f1062f8e7ea23aebcd3c3c5f0ef6f8845292
Otavio Salvador <otavio@ossystems.com.br>
2024-09-16 18:58:13 -0300

    fff
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- users/otavio/home/micro.nix
+++ users/otavio/home/micro.nix
@@ -61,22 +61,4 @@
     slack
     tdesktop
   ];
-
-  systemd.user.services = {
-    xwaylandvideobridge = {
-      Unit = {
-        Description = "Tool to make it easy to stream wayland windows and screens to existing applications running under Xwayland";
-      };
-
-      Service = {
-        Type = "simple";
-        ExecStart = "${pkgs.xwaylandvideobridge}/bin/xwaylandvideobridge";
-        Restart = "on-failure";
-      };
-
-      Install = {
-        WantedBy = [ "sway-session.target" ];
-      };
-    };
-  };
 }
