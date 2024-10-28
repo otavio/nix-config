cf2262dfb988e4265fd039f3b51c4f29d2550367
Otavio Salvador <otavio@ossystems.com.br>
2024-10-28 12:05:28 -0300

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'disko':
        'github:nix-community/disko/b6215392ec3bd05e9ebfbb2f7945c414096fce8f' (2024-10-12)
      → 'github:nix-community/disko/3163b2724c6adec071ce317e038ae71ce3ab5974' (2024-10-28)
    • Updated input 'emacs-overlay':
        'github:nix-community/emacs-overlay/5921ff72aa8019353091f0e63b3e34db28fa757f' (2024-10-12)
      → 'github:nix-community/emacs-overlay/b32650fe39381fd539d5452962adb3e37d045c62' (2024-10-28)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/d57112db877f07387ce7104b5ac346ede556d2d7' (2024-10-12)
      → 'github:nix-community/home-manager/e83414058edd339148dc142a8437edb9450574c8' (2024-10-27)
    • Updated input 'nixos-hardware':
        'github:NixOS/nixos-hardware/a8dd1b21995964b115b1e3ec639dd6ce24ab9806' (2024-10-12)
      → 'path:/nix/store/j03vrn9fc60k6a5ac9293fpzv5sz4x7p-source?lastModified=1728729581&narHash=sha256-oazkQ/z7r43YkDLLQdMg8oIB3CwWNb%2B2ZrYOxtLEWTQ%3D&rev=a8dd1b21995964b115b1e3ec639dd6ce24ab9806' (2024-10-12)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/5633bcff0c6162b9e4b5f1264264611e950c8ec7' (2024-10-09)
      → 'github:NixOS/nixpkgs/18536bf04cd71abd345f9579158841376fdd0c5a' (2024-10-25)
    • Updated input 'sops-nix':
        'github:Mic92/sops-nix/06535d0e3d0201e6a8080dd32dbfde339b94f01b' (2024-10-08)
      → 'github:Mic92/sops-nix/1666d16426abe79af5c47b7c0efa82fd31bf4c56' (2024-10-27)

--- hosts/features/required/nix.nix
+++ hosts/features/required/nix.nix
@@ -20,7 +20,7 @@
 
       trusted-users = [ "root" "@wheel" ];
       auto-optimise-store = lib.mkDefault true;
-      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      experimental-features = [ "nix-command" "flakes" ];
       warn-dirty = false;
     };
 
