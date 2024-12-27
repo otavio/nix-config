8aa178b4c78ff8e09f164484ac96a167da21e08a
Otavio Salvador <otavio@ossystems.com.br>
2024-12-27 14:39:46 -0300

    hosts: micro: add Keystone 3 Pro rule
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- hosts/micro/default.nix
+++ hosts/micro/default.nix
@@ -51,6 +51,9 @@
     ACTION=="add|change", KERNEL=="sd[a-z]|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="mq-deadline"
     # Set scheduler for rotating disks
     ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"
+
+    # Keystone 3 Pro
+    ATTRS{idVendor}=="1209", ATTRS{idProduct}=="3001", MODE:="0666", ENV{ID_MM_DEVICE_IGNORE}="1", ENV{ID_MM_PORT_IGNORE}="1"
   '';
 
   services.xserver = {
