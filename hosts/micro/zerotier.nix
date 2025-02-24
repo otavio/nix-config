d1607102372b1a05452c8b9c77d55c4391fe5afd
Otavio Salvador <otavio@ossystems.com.br>
2025-02-24 20:53:32 -0300

    micro: add Salvador's Home network for zerotier
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- hosts/micro/zerotier.nix
+++ hosts/micro/zerotier.nix
@@ -4,13 +4,16 @@
     joinNetworks = [
       # O.S. Systems
       "5e6f75af38d42102"
+
+      # Salvador's Home
+      "5e6f75af38991ce8"
     ];
   };
 
   systemd.services.zerotierone-dns = {
-    after = [ "sys-devices-virtual-net-zt6fl2ddr2.device" ];
-    bindsTo = [ "sys-devices-virtual-net-zt6fl2ddr2.device" ];
-    wantedBy = [ "sys-devices-virtual-net-zt6fl2ddr2.device" ];
+    after = [ "sys-devices-virtual-net-zt6fl2ddr2.device" "sys-devices-virtual-net-zt6fl6zm6q.device" ];
+    bindsTo = [ "sys-devices-virtual-net-zt6fl2ddr2.device" "sys-devices-virtual-net-zt6fl6zm6q.device" ];
+    wantedBy = [ "sys-devices-virtual-net-zt6fl2ddr2.device" "sys-devices-virtual-net-zt6fl6zm6q.device" ];
     description = "zerotier dns";
     serviceConfig = {
       RemainAfterExit = true;
@@ -18,6 +21,10 @@
         resolvectl dns    zt6fl2ddr2 10.5.1.254
         resolvectl domain zt6fl2ddr2 "~lab.ossystems"
         resolvectl dnssec zt6fl2ddr2 false
+
+        resolvectl dns    zt6fl6zm6q 10.4.0.254
+        resolvectl domain zt6fl6zm6q "~casa.salvador"
+        resolvectl dnssec zt6fl6zm6q false
       '';
     };
   };
