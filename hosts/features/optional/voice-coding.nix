53babec17d836f53691419fe9e5670fdcc6647fd
Otavio Salvador <otavio@ossystems.com.br>
2026-03-30 10:26:28 -0300

    refactor(voice-coding): replace talon-nix module with inline udev rules
    Replace the talon-nix NixOS module import with inline Tobii eye tracker
    udev rules extracted from the Talon installation.

--- hosts/features/optional/voice-coding.nix
+++ hosts/features/optional/voice-coding.nix
@@ -1,11 +1,15 @@
-{ inputs, ... }:
+{ ... }:
 
 {
-  imports = [ inputs.talon-nix.nixosModules.talon ];
-
-  nixpkgs.overlays = [
-    inputs.talon-nix.overlays.default
-  ];
-
-  programs.talon.enable = true;
+  # Udev rules for Talon voice coding (Tobii eye tracker support)
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="0127", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="0118", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="0106", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="0128", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="010a", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="0102", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="0313", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="2104", ATTRS{idProduct}=="0318", TAG+="uaccess"
+  '';
 }
