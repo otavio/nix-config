0c4f6545a4c2718a9483789f64f6878c7233df36
Otavio Salvador <otavio@ossystems.com.br>
2026-04-20 10:14:51 -0300

    refactor: enable home-manager-switch now that the Nix bug is fixed
    The commented-out import referenced a Nix 1.19.2 symlink bug; installed
    Nix is 2.34.6, so the blocker is gone. Wire up the feature module
    directly so `home-manager switch` works against the live nix-config.

--- users/otavio/home/generic.nix
+++ users/otavio/home/generic.nix
@@ -1,14 +1,7 @@
 {
   imports = [
     ./features/global
-
-    # Nix 1.19.2 fails to build a derivation containing a symlink to store path.
-    #
-    # Refs:
-    # - https://github.com/nix-community/home-manager/issues/4692
-    # - https://github.com/NixOS/nix/issues/9579
-    #./features/home-manager-switch
-
+    ./features/home-manager-switch
     ./features/emacs
   ];
 }
