ea5593a7c4b2901af4acbc35fe0dfe1b29b3f2e7
Otavio Salvador <otavio@ossystems.com.br>
2026-04-28 16:02:08 -0300

    feat: enable xdg-desktop-portal with GTK backend on i3wm
    - Enable xdg-desktop-portal in the desktop feature with the GTK portal as
      the common default so apps relying on portals (file pickers, screen
      sharing, opening URIs) work outside of GNOME/KDE sessions.
    - Export XDG_SESSION_TYPE=x11 in the i3wm xinitrc and propagate it to the
      systemd user manager and dbus activation environment, so portal services
      pick the right backend for the running X11 session.

--- hosts/features/optional/desktop.nix
+++ hosts/features/optional/desktop.nix
@@ -1,7 +1,15 @@
+{ pkgs, ... }:
+
 {
   hardware.graphics.enable = true;
 
   programs.dconf.enable = true;
 
   services.gnome.gnome-keyring.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config.common.default = "gtk";
+  };
 }
