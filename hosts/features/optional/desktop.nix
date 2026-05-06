504c1db4f60e9dc9abfc0fb379225c156f0e6d1e
Otavio Salvador <otavio@ossystems.com.br>
2026-05-06 10:13:44 -0300

    Revert "feat: enable xdg-desktop-portal with GTK backend on i3wm"
    This reverts commit ea5593a7c4b2901af4acbc35fe0dfe1b29b3f2e7.

--- hosts/features/optional/desktop.nix
+++ hosts/features/optional/desktop.nix
@@ -1,15 +1,7 @@
-{ pkgs, ... }:
-
 {
   hardware.graphics.enable = true;
 
   programs.dconf.enable = true;
 
   services.gnome.gnome-keyring.enable = true;
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-    config.common.default = "gtk";
-  };
 }
