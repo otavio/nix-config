d4de7ea2e55fe8be9db3a4f78695002e21f25526
Otavio Salvador <otavio@ossystems.com.br>
2026-09-01 11:27:42 -0300

    fix(micro): point open-windoze at the win11 domain
    The Windoze guest has been superseded by a new win11 VM, so the script kept
    starting and attaching to the retired machine.

--- users/otavio/home/micro.nix
+++ users/otavio/home/micro.nix
@@ -43,9 +43,9 @@
       name = "open-windoze";
       runtimeInputs = with pkgs; [ virt-viewer ];
       text = ''
-        virsh -c qemu:///system list --all --state-running --name | grep -q "Windoze" \
-          || virsh -c qemu:///system start Windoze \
-          && exec virt-viewer -f -c qemu:///system Windoze
+        virsh -c qemu:///system list --all --state-running --name | grep -q "win11" \
+          || virsh -c qemu:///system start win11 \
+          && exec virt-viewer -f -c qemu:///system win11
       '';
     })
 
